5587d092c5366fab337606a1cc61b2ed8b84bd5d
radityaprambudi <brakedlimited@gmail.com>
2025-07-11 22:35:55 +0700

    Tambah fitur dashboard: tambah transaksi dan hitung saldo

---
+++ js/dashboard.js
@@ -0,0 +1,43 @@
+if (localStorage.getItem("isLoggedin") !== "true") {
+    window.location.href = "index.html";
+}
+
+const form = document.getElementById("form-transaksi");
+const daftar = document.getElementById("daftar-transaksi");
+const saldoDisplay = document.getElementById("saldo");
+
+let transaksi = JSON.parse(localStorage.getItem("transaksi")) || [];
+
+function render() {
+    daftar.innerHTML = "";
+    let saldo = 0;
+
+    transaksi.forEach((t, i) => {
+        const li = document.createElement("li");
+        li.textContent = `${t.jenis} - ${t.keterangan} - Rp${t.jumlah}`;
+        daftar.appendChild(li);
+
+        saldo += t.jenis === "pemasukan" ? +t.jumlah : -t.jumlah;
+    });
+
+    saldoDisplay.textContent = saldo;
+    localStorage.setItem("transaksi", JSON.stringify(transaksi));
+}
+
+form.onsubmit = function (e) {
+    e.preventDefault();
+    const jenis = document.getElementById("jenis").value;
+    const keterangan = document.getElementById("keterangan").value;
+    const jumlah = document.getElementById("jumlah").value;
+
+    transaksi.push({ jenis, keteranga, jumlah});
+    render();
+    form.reset();
+};
+
+function logout() {
+    localStorage.removeItem("isLoggedIn");
+    window.location.href = "index.html";
+}
+
+render();
---
+++ js/login.js
@@ -0,0 +1,11 @@
+function login() {
+    const username = document.querySelector('input[type="text"]').value;
+    const password = document.querySelector('input[type="password"]').value;
+
+    if (username === "admin" && password === "admin") {
+        localStorage.setItem("isLoggedIn", "true");
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Loggin Gagal!");
+    }
+}
