99450ecc39d7c58d864d8183b99516a1995bd64d
radityaprambudi <brakedlimited@gmail.com>
2025-07-11 22:53:18 +0700

    Auto update 22:53:18

--- js/login.js
+++ js/login.js
@@ -2,6 +2,8 @@ function login() {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
+    console.log("auto push test")
+
     if (username === "admin" && password === "admin") {
         localStorage.setItem("isLoggedIn", "true");
         window.location.href = "dashboard.html";
